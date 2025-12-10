112b7025ce532ea567ee4b6fb3f53dc78d017d21
danchik <danikshved31@gmail.com>
2025-12-10 16:33:32 +0300

    Update collision.py

--- collision/collision.py
+++ collision/collision.py
@@ -1,1 +1,9 @@
-
+def isCorrectRect(points):
+    if len(points) != 2:
+        return False
+    
+    try:
+        (x1, y1), (x2, y2) = points
+        return x1 < x2 and y1 < y2
+    except (ValueError, TypeError):
+        return False
